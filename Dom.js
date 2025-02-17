300fda455f6f60afed5266d0066d5f0e67dd130a
kidfromcalifornia <hello.jonnyhicks@gmail.com>
2025-02-17 21:50:55 +0100

    JS file and hamburger menu DOM.  acrtion works but Need to fix why you cant see Ham-menu to close.

---
+++ Dom.js
@@ -0,0 +1,7 @@
+
+const hamMenu = document.querySelector('.ham-menu')
+const offScreenMenu = document.querySelector('.ham-offscreen')
+hamMenu.addEventListener('click', () => {
+  hamMenu.classList.toggle('active')
+  offScreenMenu.classList.toggle('active')
+})
