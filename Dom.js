2643e59a88f7a35a2153283c9a6f8adf3fd47b6c
kidfromcalifornia <hello.jonnyhicks@gmail.com>
2025-02-18 13:20:18 +0100

    added svg img ticket on form-

--- Dom.js
+++ Dom.js
@@ -1,7 +1,10 @@
 
+
 const hamMenu = document.querySelector('.ham-menu')
 const offScreenMenu = document.querySelector('.ham-offscreen')
 hamMenu.addEventListener('click', () => {
   hamMenu.classList.toggle('active')
   offScreenMenu.classList.toggle('active')
-})
+}
+)
+
